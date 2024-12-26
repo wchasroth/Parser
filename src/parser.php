7e827adb3b278113c1f8084b045d2707a9e9bd5b
Charles Roth <croth@thedance.net>
2024-12-26 13:51:31 -0500

    Start actual work on parsing county-party information from MDP site

--- src/parser.php
+++ src/parser.php
@@ -1,9 +1,7 @@
 <?php
-   // Mostly-empty template for a generic parsing program.
-   // E.g. it reads from stdin, does stuff, and writes to stdout.
+   // Parse county name, website, facebook, and email address from
+   // MDP county party list at https://michigandems.com/county-chairs
    //
-   // (Using composer to get whatever packages it needs -- in this
-   // case, using functions from the Alfred/Str class.)
 
    declare(strict_types=1);
 
@@ -13,8 +11,22 @@
 
    require_once('../vendor/autoload.php');
 
+   $state = 0;  // outside of county block
+
    while ( ($line = fgets(STDIN)) !== false) {
-      echo Str::contains($line, "<h2") ? "H " : "  ";
-      echo $line;
+      if ($state == 0) {
+         if (Str::contains($line, '<h2 class="elementor-heading-title elementor-size-default')) {
+            if (Str::contains($line, " County<")) {
+               $state == 1;  // started county block
+               $line = Str::replaceAll($line, "<BR>", "");
+               $name = Str::substringBefore   ($line, " County<");
+               $name = Str::substringAfterLast($name, ">");
+               $name = trim($name);
+               echo "$name\n";
+            }
+         }
+         $state = 0;
+      }
    }
-   
+
+   //<h2 class="elementor-heading-title elementor-size-default"><a href="https://www.allegandems.com" target="_blank">Allegan County</a></h2>				</div>
