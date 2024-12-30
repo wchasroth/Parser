558e183cbf57e29b9848849b7336b9f328639cf2
Charles Roth <croth@thedance.net>
2024-12-30 16:34:16 -0500

    Add SQL to create county_contacts table

---
+++ SQL/00-createCountyContacts.sql
@@ -0,0 +1,96 @@
+DROP TABLE IF EXISTS county_contacts;
+
+CREATE TABLE county_contacts (
+   id   tinyint          NOT NULL DEFAULT  0,  PRIMARY KEY(id),
+   web1      varchar(60) NOT NULL DEFAULT "",
+   web2      varchar(60) NOT NULL DEFAULT "",
+   facebook  varchar(60) NOT NULL DEFAULT "",
+   twitter   varchar(60) NOT NULL DEFAULT "",
+   instagram varchar(60) NOT NULL DEFAULT "",
+   email     varchar(60) NOT NULL DEFAULT "",
+   volunteer varchar(60) NOT NULL DEFAULT ""
+);
+
+INSERT INTO county_contacts (id) values (1);
+INSERT INTO county_contacts (id) values (2);
+INSERT INTO county_contacts (id) values (3);
+INSERT INTO county_contacts (id) values (4);
+INSERT INTO county_contacts (id) values (5);
+INSERT INTO county_contacts (id) values (6);
+INSERT INTO county_contacts (id) values (7);
+INSERT INTO county_contacts (id) values (8);
+INSERT INTO county_contacts (id) values (9);
+INSERT INTO county_contacts (id) values (10);
+INSERT INTO county_contacts (id) values (11);
+INSERT INTO county_contacts (id) values (12);
+INSERT INTO county_contacts (id) values (13);
+INSERT INTO county_contacts (id) values (14);
+INSERT INTO county_contacts (id) values (15);
+INSERT INTO county_contacts (id) values (16);
+INSERT INTO county_contacts (id) values (17);
+INSERT INTO county_contacts (id) values (18);
+INSERT INTO county_contacts (id) values (19);
+INSERT INTO county_contacts (id) values (20);
+INSERT INTO county_contacts (id) values (21);
+INSERT INTO county_contacts (id) values (22);
+INSERT INTO county_contacts (id) values (23);
+INSERT INTO county_contacts (id) values (24);
+INSERT INTO county_contacts (id) values (25);
+INSERT INTO county_contacts (id) values (26);
+INSERT INTO county_contacts (id) values (27);
+INSERT INTO county_contacts (id) values (28);
+INSERT INTO county_contacts (id) values (29);
+INSERT INTO county_contacts (id) values (30);
+INSERT INTO county_contacts (id) values (31);
+INSERT INTO county_contacts (id) values (32);
+INSERT INTO county_contacts (id) values (33);
+INSERT INTO county_contacts (id) values (34);
+INSERT INTO county_contacts (id) values (35);
+INSERT INTO county_contacts (id) values (36);
+INSERT INTO county_contacts (id) values (37);
+INSERT INTO county_contacts (id) values (38);
+INSERT INTO county_contacts (id) values (39);
+INSERT INTO county_contacts (id) values (40);
+INSERT INTO county_contacts (id) values (41);
+INSERT INTO county_contacts (id) values (42);
+INSERT INTO county_contacts (id) values (43);
+INSERT INTO county_contacts (id) values (44);
+INSERT INTO county_contacts (id) values (45);
+INSERT INTO county_contacts (id) values (46);
+INSERT INTO county_contacts (id) values (47);
+INSERT INTO county_contacts (id) values (48);
+INSERT INTO county_contacts (id) values (49);
+INSERT INTO county_contacts (id) values (50);
+INSERT INTO county_contacts (id) values (51);
+INSERT INTO county_contacts (id) values (52);
+INSERT INTO county_contacts (id) values (53);
+INSERT INTO county_contacts (id) values (54);
+INSERT INTO county_contacts (id) values (55);
+INSERT INTO county_contacts (id) values (56);
+INSERT INTO county_contacts (id) values (57);
+INSERT INTO county_contacts (id) values (58);
+INSERT INTO county_contacts (id) values (59);
+INSERT INTO county_contacts (id) values (60);
+INSERT INTO county_contacts (id) values (61);
+INSERT INTO county_contacts (id) values (62);
+INSERT INTO county_contacts (id) values (63);
+INSERT INTO county_contacts (id) values (64);
+INSERT INTO county_contacts (id) values (65);
+INSERT INTO county_contacts (id) values (66);
+INSERT INTO county_contacts (id) values (67);
+INSERT INTO county_contacts (id) values (68);
+INSERT INTO county_contacts (id) values (69);
+INSERT INTO county_contacts (id) values (70);
+INSERT INTO county_contacts (id) values (71);
+INSERT INTO county_contacts (id) values (72);
+INSERT INTO county_contacts (id) values (73);
+INSERT INTO county_contacts (id) values (74);
+INSERT INTO county_contacts (id) values (75);
+INSERT INTO county_contacts (id) values (76);
+INSERT INTO county_contacts (id) values (77);
+INSERT INTO county_contacts (id) values (78);
+INSERT INTO county_contacts (id) values (79);
+INSERT INTO county_contacts (id) values (80);
+INSERT INTO county_contacts (id) values (81);
+INSERT INTO county_contacts (id) values (82);
+INSERT INTO county_contacts (id) values (83);
--- src/parser.php
+++ src/parser.php
@@ -13,13 +13,17 @@
 
    $county   = "";
    $lastLink = "";
+   $web1Found = false;
    while ( ($line = fgets(STDIN)) !== false) {
       if (Str::contains($line, '<h2 class="elementor-heading-title elementor-size-default')) {
          if (Str::contains($line, " County<")) {
             $line = Str::replaceAll($line, "<BR>", "");
+            $line = Str::replaceAll($line, "<br>", "");
             $name = Str::substringBefore   ($line, " County<");
             $name = Str::substringAfterLast($name, ">");
             $county = trim($name);
+            $county = Str::replaceAll($county, ".", "");
+            $web1Found = false;
          }
       }
 
@@ -27,23 +31,50 @@
           $link = extractHyperlink($line);
           if (! empty($link)  &&  ! similarUrls($link, $lastLink)) {
               if ($county == "Wexford"  &&  Str::contains($link, "secure.actblue"))  break;  // DONE!
-              echo "$county  $link\n";
+
+              $column = getColumnFor($link);
+              if ($column == "web") {
+                  $column = ($web1Found ? "web2" : "web1");
+                  $web1Found = true;
+              }
+              $sql = generateUpdateSql($county, $column, $link);
+              echo "$sql\n";
               $lastLink = $link;
           }
       }
    }
 
+   function generateUpdateSql (string $county, string $column, string $link): string {
+       return "UPDATE county_contacts SET $column = '$link' WHERE id = (SELECT id FROM county WHERE name='$county');";
+   }
+
+   function getColumnFor (string $link): string {
+       $column = "email";
+       if (Str::contains($link, "http")) {
+           if      (Str::contains($link, "facebook"))  $column = "facebook";
+           else if (Str::contains($link, "twitter"))   $column = "twitter";
+           else if (Str::contains($link, "instagram")) $column = "instagram";
+           else                                        $column = "web";
+       }
+       return $column;
+   }
+
    function extractHyperlink(string $line): string {
-      if      (Str::contains($line,"http://"))   $protocol = "http://";
-      else if (Str::contains($line,"https://"))  $protocol = "https://";
+      if      (Str::contains($line, "http://"))   $protocol = "http://";
+      else if (Str::contains($line, "https://"))  $protocol = "https://";
+      else if (Str::contains($line, "mailto:"))   $protocol = "mailto:";
       else return "";
 
       $text = Str::substringAfter ($line, $protocol);
       $text = Str::substringBefore($text, '"');
       $text = rtrim($text, "/");
+      $text = Str::replaceAll($text, "%20", "");
       if (Str::contains($text, "www.w3.org/2000/svg"))   return "";
       if (Str::contains($text, ".pdf"))                  return "";
-      if (Str::contains($text, "@gmail"))                return "";
+      if (Str::contains($protocol, "http")  &&  Str::contains($text, "@gmail")) {
+          fwrite(STDERR, "Error: $protocol $text\n");
+          return "";
+      }
       return "$protocol$text";
    }
 
